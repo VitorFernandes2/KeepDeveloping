422bf038d4bd43c984322a4c2d344516393330d1
Luís António Moreira Ferreira da Silva <44380629+Luisilva99@users.noreply.github.com>
2022-01-05 21:09:02 +0000

    Feature
    - HomeScreen design remodeled;
    - Introduction of Expo-Font library;
    - Components design remodeled.

--- KeepDeveloping/App.js
+++ KeepDeveloping/App.js
@@ -7,12 +7,14 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{
-        headerShown: false
-      }} initialRouteName="Home">
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+        initialRouteName="Home"
+      >
         <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
--- KeepDeveloping/components/TransparentButton.js
+++ KeepDeveloping/components/TransparentButton.js
@@ -17,18 +17,18 @@ const TransparentButton = ({txt, clicked}) => {
 const styles = StyleSheet.create({
     bt:{
         backgroundColor: "#00000000",
-        borderRadius: 10,
+        borderRadius: 50,
         borderColor: "#43136e",
         borderWidth: 1,
         width: "75%",
         flexDirection: 'row',
-        justifyContent: 'center',
-        margin: 16,
         padding: 6
     },
     text:{
         color: "#43136e",
-        fontSize: 20
+        fontSize: 20,
+        flex: 1,
+        textAlign: 'center',
     }
 })
 
