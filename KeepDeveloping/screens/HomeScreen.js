723f9374b982e7bc300a4c3339e93c2d426fe93d
Luís António Moreira Ferreira da Silva <44380629+Luisilva99@users.noreply.github.com>
2022-01-06 00:02:12 +0000

    Feature Update
    - Screen Link update.

--- KeepDeveloping/screens/HomeScreen.js
+++ KeepDeveloping/screens/HomeScreen.js
@@ -26,10 +26,9 @@ const HomeScreen = ({ navigation }) => {
         <Text style={styles.textSlogan}>Let's Agile!</Text>
       </View>
       <View style={styles.containerButtons}>
-        <FullButton clicked={click} txt="Jogar" />
-        <FullButton clicked={click} txt="Documentação" />
+        <FullButton clicked={() => navigation.navigate('',{})} txt="Jogar" />
+        <FullButton clicked={() => navigation.navigate('',{})} txt="Documentação" />
         <TransparentButton clicked={()=>navigation.navigate("InformationScreen")} txt="Informação"/>
-        <TransparentButton clicked={() => navigation.navigate('',{})} txt="Informação" />
       </View>
     </View>
   );
